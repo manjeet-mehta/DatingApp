068986d8bb820fe372f210fe42d38f62b4d7e032
Manjeet Mehta <MY_NAME@example.com>
2021-01-31 01:32:37 +0530

    added dropdown

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,9 +16,12 @@ import { NavComponent } from './nav/nav.component';
   ],
   imports: [
     BrowserModule,  
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule 
+    FormsModule,
+    BsDropdownModule.forRoot()
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
