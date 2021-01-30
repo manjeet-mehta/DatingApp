910ee1d95c55d786b25b075d7e5488ee13ffbcb9
Manjeet Mehta <MY_NAME@example.com>
2021-01-31 02:56:55 +0530

    added landing/home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,  
