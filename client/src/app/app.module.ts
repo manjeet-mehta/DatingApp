04a0e9c884e468778e334c9474b9abfe404efddb
Manjeet Mehta <MY_NAME@example.com>
2021-01-30 22:37:44 +0530

    added first service to serve login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -13,7 +15,8 @@ import { NavComponent } from './nav/nav.component';
   imports: [
     BrowserModule,  
     AppRoutingModule,
-    HttpClientModule 
+    HttpClientModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
